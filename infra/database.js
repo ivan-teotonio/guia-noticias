2a19ebe2bfbbdf4f9c7f78172071f4acadbe9258
Ivan <itaj@discente.ifpe.edu.br>
2025-10-19 04:00:25 +0000

    Finaliza o endpoint de status

--- infra/database.js
+++ infra/database.js
@@ -9,9 +9,15 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   }); // 1️⃣ cria um novo cliente para se conectar ao banco
   await client.connect(); // 2️⃣ abre a conexão (operação assíncrona)
-  const result = await client.query(queryObject); // 3️⃣ executa a query (operação assíncrona)
-  await client.end(); // 4️⃣ fecha a conexão (operação assíncrona)
-  return result; // 5️⃣ retorna o resultado
+
+  try {
+    const result = await client.query(queryObject); // 3️⃣ executa a query (operação assíncrona)
+    return result; // 5️⃣ retorna o resultado
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end(); // 4️⃣ fecha a conexão (operação assíncrona)
+  }
 }
 
 export default {

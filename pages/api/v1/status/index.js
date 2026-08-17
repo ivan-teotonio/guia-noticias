5bdb0bedb66671d28a94f3c12697906b44ac8a24
Ivan <itaj@discente.ifpe.edu.br>
2026-08-17 01:36:47 +0000

    Finaliza o

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,36 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ status: "São acima de média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConexionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConexionsValue =
+    databaseMaxConexionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenConnectionResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname= $1;",
+    values: [databaseName],
+  });
+  const databaseOpenConnectionValue =
+    databaseOpenConnectionResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConexionsValue),
+        open_connections: databaseOpenConnectionValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  //
+  const responseBody = await response.json();
+
+  // checa se o campo updated_at é uma string válida de data
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); // Check if it's a valid date
+  // checa se o campo updated_at é igual ao valor esperado
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  // checa se o campo dependecies.database.version está presente e é uma string
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.open_connections).toEqual(1);
 });
